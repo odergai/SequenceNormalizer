36b12fae75cf998df653db62a691701f12ea6548
odergai <43633810+odergai@users.noreply.github.com>
2019-03-07 10:40:02 +0100

    Update seq_rearrange2.py

--- seq_rearrange2.py
+++ seq_rearrange2.py
@@ -1,5 +1,5 @@
 __author__ = 'odergai'
-"""seq_rearrange takes as an argument string containing proteins sequence
+"""seq_rearrange2 takes as an argument string containing proteins sequence
     and return length of minimal sub-sequence that need to be substituted
     to have all amino acids equally represented.
     Args:
